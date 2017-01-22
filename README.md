4e3ae499f29fbdd36d9764401913b2285afdb1c8
S.D. Michaud <smich010@uottawa.ca>
2017-01-22 11:26:24 -0500

    Initial Commit
    Add sprites and inital base files.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pokémon Novelty Integer System (PNIS)
+
+## About this project
+This repository holds the files used to run a demonstration of PNIS in action at [sdmsolutions.ca](http://sdmsolutions.ca)
+
+## What is PNIS?
+The Pokémon Novelty Integer System is a numeral system that uses 803 as its base, as opposed to the most common decimal system which uses 10 as its base.
+
+## How does PNIS work?
+The decimal system has 10 numerals:
+```
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+```
+The PNIS has 803 numerals, represented by each Pokémon.
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SDM Solutions</title>
+    <link rel="stylesheet" type="text/css" href="styles/pnis_style.css">
+    <!--Include for jquery, hosted by google-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</head>
+<body>
+    <div id="jquery_test"></div>
+    <script type="text/javascript">
+        $('#jquery_test').text('Hello World!');
+    </script>
+    <noscript>Please enable JavaScript from googleapis.com and sdmsolutions.ca</noscript>
+    <!--TODO Add forms for converting decimal to pnis-->
+    <!--TODO Add php script for converting decimal to pnis-->
+    <!--TODO Add text and images to make site look good-->
+    <!--IDEA Create an Input Method Editor (IME) for typing in pnis-->
+</body>
+</html>
